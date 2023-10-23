a97da9839844f13aef0be2ddb3d30e10cde362e2
Jose Daniel <jose_daniel@thewisedreams.com>
2023-10-23 10:03:49 +0100

    delete comments that not is mine

--- routes/images.js
+++ routes/images.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Image = require("../models/image");
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
 
 //GET ALL
 router.get("/", async (req, res) => {
